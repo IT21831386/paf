bdac72863eb5c75752c82192cd1e69434621c2fe
lasitha <lasitharathnayaka580@gmail.com>
2026-04-23 15:50:21 +0530

    add Notification REST controller

---
+++ backend/src/main/java/com/smartcampus/notification/model/NotificationType.java
@@ -0,0 +1,12 @@
+package com.smartcampus.notification.model;
+
+public enum NotificationType {
+    BOOKING_APPROVED,
+    BOOKING_REJECTED,
+    TICKET_STATUS,
+    TICKET_ASSIGNED,
+    NEW_COMMENT,
+    VISITOR_APPROVED,
+    VISITOR_REJECTED,
+    GENERAL
+}
